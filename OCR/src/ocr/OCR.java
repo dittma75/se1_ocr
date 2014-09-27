06a7d02f64ef401854ad026d910f8795a2067d3e
dittma75 <>
2014-09-27 12:37:04 -0400

    Added XML parsing and a sample XML file.

--- OCR/src/ocr/OCR.java
+++ OCR/src/ocr/OCR.java
@@ -1,5 +1,9 @@
 package ocr;
-import java.io.File;
+import java.io.BufferedWriter;
+import java.io.File;  
+import java.io.FileWriter;  
+import java.io.IOException;
+import java.util.Scanner;
 import net.sourceforge.tess4j.*;
 /**
  * Handle the Optical Character Recognition processing for FAA pdf files.
@@ -11,16 +15,71 @@ public class OCR
         String property = System.getProperty("sun.arch.data.model");
         System.setProperty("jna.library.path", 
             "32".equals(property) ? "lib/win32-x86" : "lib/win32-x86-64");
-        File imageFile = new File("resources/ACY/00669ATLANTICCITY.pdf");
+        File imageFile = new File("resources/ATL/00026AD.pdf");
         Tesseract instance = Tesseract.getInstance();  // JNA Interface Mapping
         //Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
 
         try {
             String result = instance.doOCR(imageFile);
-            System.out.println(result);
+            result = result.replaceAll("^(\\w)", "");
+            formatOutput(result);
+            //System.out.println(result);
         } catch (TesseractException e) {
             System.err.println(e.getMessage());
         }
     }
-    
+    public static void formatOutput(String tess_out)
+    {
+        Scanner scanner = new Scanner(tess_out);
+        String next_line;
+        boolean header_finished = false;
+        boolean instructions_finished = false;
+        String xml_file = "<plate>\n<heading>\n";
+        while (scanner.hasNextLine() && !header_finished)
+        {
+            next_line = scanner.nextLine();
+            xml_file += next_line + "\n";
+            //Header is done if we see DEPARTURE, DIAGRAM, or CITY, STATE
+            if (next_line.contains("DEPARTURE") ||             
+                next_line.contains("DIAGRAM") ||
+                next_line.matches(".*[a-zA-Z]+, [a-zA-Z]+.*"))
+            {
+                header_finished = true;
+            }
+        }
+        xml_file += "</heading>\n<instructions>\n";
+        //We want to start off blank so we can save the last line for
+        //special instructions.
+        next_line = "";
+        while (scanner.hasNextLine() && !instructions_finished)
+        {
+            xml_file += next_line + "\n";
+            next_line = scanner.nextLine();
+            if (next_line.contains("CAUTION") ||
+                next_line.contains("SPECIAL"))
+            {
+                instructions_finished = true;
+            }
+        }
+        xml_file += "</instructions>\n<special>\n";
+        //This contains the special instructions piece found earlier.
+        xml_file += next_line + "\n";
+        while (scanner.hasNextLine())
+        {
+            xml_file += scanner.nextLine() + "\n";
+        }
+        xml_file += "</special>\n</plate>";
+        xml_file = xml_file.replaceAll("\n *", "\n");
+        xml_file = xml_file.replaceAll("\n+", "\n");
+        //Write XML file
+        try {
+          File file = new File("file_name.xml");
+          BufferedWriter output = new BufferedWriter(new FileWriter(file));
+          output.write(xml_file);
+          output.close();
+        } catch ( IOException e ) {
+           e.printStackTrace();
+        }
+
+    }
 }
